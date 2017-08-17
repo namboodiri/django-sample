ac392261c86f0d2972cc3e6dec34d95a23a6335f
Sankaran Namboodiri <san@rideblock.com>
2017-08-17 13:27:07 +0530

    added login url

--- djangosample/urls.py
+++ djangosample/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth.views import login
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^login/$', login, {'template_name': 'login.html'}, name='login'),
 ]
+
